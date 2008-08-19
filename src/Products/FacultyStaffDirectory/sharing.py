050101709f6f43774c8d6dfcdc7ead2e094ea4a4
ems174 <ems174@dce.psu.edu>
2008-08-19 14:28:49 +0000

    Fail nicely in 2.5.x.

--- src/Products/FacultyStaffDirectory/sharing.py
+++ src/Products/FacultyStaffDirectory/sharing.py
@@ -1,8 +1,16 @@
-from zope.interface import implements
-from plone.app.workflow.interfaces import ISharingPageRole
+from zope.interface import implements, Interface
 from Products.CMFPlone import PloneMessageFactory as _
 
+try:
+    from plone.app.workflow.interfaces import ISharingPageRole as interfaceToImplement
+except ImportError:
+    # Fail nicely, this version of Plone doesn't know anything about @@sharing page roles.
+    class IDoNothing(Interface):
+        pass
+    interfaceToImplement = IDoNothing
+
 class PersonnelManagerRole(object):
-   implements(ISharingPageRole)
-   title = _(u"title_can_manage_personnel", default=u"Can manage personnel")
-   required_permission = 'Manage portal content'
+    implements(interfaceToImplement)
+    title = _(u"title_can_manage_personnel", default=u"Can manage personnel")
+    required_permission = 'Manage portal content'
+    
