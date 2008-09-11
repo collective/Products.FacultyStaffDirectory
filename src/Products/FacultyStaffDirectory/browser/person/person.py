48d3458479dd659e09f5cd420c4f09ffb80d832c
Eric Steele <ems174@psu.edu>
2008-09-11 16:05:24 +0000

    Moving python methods out of the template and into the view.

--- src/Products/FacultyStaffDirectory/browser/person/person.py
+++ src/Products/FacultyStaffDirectory/browser/person/person.py
@@ -2,4 +2,21 @@ from Products.Five import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 class PersonView(BrowserView):
-    pass
+    
+    @property
+    def portrait(self):
+        """Return None if no image. 
+        
+        (tag() field will return an img tag regardless)
+        
+        """
+        return self.context.getImage() and self.context.getWrappedField('image').tag(self.context, scale='normal')
+    
+    @property
+    def officeAddress(self):
+        return self.context.getOfficeAddress().replace('\n', '<br />')
+        
+    @property
+    def email(self):
+        return self.context.spamProtectFSD(self.context.getEmail())
+    
