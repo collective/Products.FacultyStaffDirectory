fa8a4d8d22a6f9183800f4fcecab23a53fd1fbca
Eric Steele <ems174@psu.edu>
2008-09-12 13:44:23 +0000

    Adding a tabular view for Specialty.

--- src/Products/FacultyStaffDirectory/browser/specialty/specialty.py
+++ src/Products/FacultyStaffDirectory/browser/specialty/specialty.py
@@ -1,5 +1,13 @@
 from Products.Five import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
-class GalleryView(BrowserView):
+class SpecialtyView(BrowserView):
+    @property
+    def subSpecialties(self):
+        return self.context.getFolderContents({'portal_type':'FSDSpecialty', 'sort_on':'sortable_title'})
+    
+class GalleryView(SpecialtyView):
+    pass
+
+class TabularView(SpecialtyView):
     pass
