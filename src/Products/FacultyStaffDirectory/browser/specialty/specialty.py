9c8f32362fc16f68bdc8276da2e14c92ed034f93
Eric Steele <ems174@psu.edu>
2008-09-12 14:32:16 +0000

    Turning the content of a specialty view into a mess of viewlets.

--- src/Products/FacultyStaffDirectory/browser/specialty/specialty.py
+++ src/Products/FacultyStaffDirectory/browser/specialty/specialty.py
@@ -1,13 +1,16 @@
 from Products.Five import BrowserView
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from plone.app.layout.viewlets.common import ViewletBase
 
 class SpecialtyView(BrowserView):
-    @property
-    def subSpecialties(self):
-        return self.context.getFolderContents({'portal_type':'FSDSpecialty', 'sort_on':'sortable_title'})
+    pass
     
 class GalleryView(SpecialtyView):
     pass
 
 class TabularView(SpecialtyView):
     pass
+    
+class SubspecialtiesViewlet(ViewletBase):
+    def getSubspecialties(self):
+        return self.context.getFolderContents({'portal_type':'FSDSpecialty', 'sort_on':'sortable_title'})
+    
--- src/Products/FacultyStaffDirectory/interfaces/browser.py
+++ src/Products/FacultyStaffDirectory/interfaces/browser.py
@@ -1,1 +1,5 @@
-#
+from zope.viewlet.interfaces import IViewletManager
+
+class ISpecialtyViewletManager(IViewletManager):
+    """Ordered viewlet manager that contains and orders parts of a specialty like body text, subspecialties, etc."""
+    pass
