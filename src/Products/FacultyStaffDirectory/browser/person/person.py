5b17af8f82f1229758e123dd31a278d891d9ddac
Eric Steele <ems174@psu.edu>
2008-09-12 20:29:54 +0000

    Moving python code to class methods.

--- src/Products/FacultyStaffDirectory/browser/person/person.py
+++ src/Products/FacultyStaffDirectory/browser/person/person.py
@@ -1,4 +1,5 @@
 from Products.Five import BrowserView
+from plone.app.layout.viewlets.common import ViewletBase
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 class PersonView(BrowserView):
@@ -20,3 +21,14 @@ class PersonView(BrowserView):
     def email(self):
         return self.context.spamProtectFSD(self.context.getEmail())
     
+
+class PersonGalleryViewlet(ViewletBase, PersonView):
+    @property
+    def portrait(self):
+        """Return None if no image. 
+        
+        (tag() field will return an img tag regardless)
+        
+        """
+        width = self.context.getClassificationViewThumbnailWidth()
+        return self.context.getImage() and self.context.getScaledImageByWidth(width)
