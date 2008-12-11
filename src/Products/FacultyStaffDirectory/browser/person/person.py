9c924d643b990c66b46e78bfb4475250c58976c4
Eric Steele <ems174@psu.edu>
2008-12-11 20:22:46 +0000

    Worked around an issue with plone viewletmanager registrations. We can't have multiple viewletmanagers registered with the same name (albeit with different contexts) if we expect to order and show/hide the included viewlets. The resolution is to create managers with different names and look up the most specific adapter of the context and IPersonViewletManager without using the name. Our lookup assumes that getAdapters() returns the most specific adapter last.

--- src/Products/FacultyStaffDirectory/browser/person/person.py
+++ src/Products/FacultyStaffDirectory/browser/person/person.py
@@ -34,13 +34,22 @@ class PersonGalleryViewlet(ViewletBase, PersonView):
         return self.context.getImage() and self.context.getScaledImageByWidth(width)
         
 class PersonGalleryView(PersonView):
-    def __init__(self, person, classification, request): # Override __init__ so we have 4 params instead of the usual 3 (since this is called using a multi-view lookup)
+    def __init__(self, person, grouping, request): # Override __init__ so we have 4 params instead of the usual 3 (since this is called using a multi-view lookup)
+        self.grouping = grouping
         super(PersonView, self).__init__(person, request)
 
     def __call__(self):
-        # import pdb; pdb.set_trace( )
+        from zope.component import getSiteManager
+        from Products.FacultyStaffDirectory.interfaces import IPersonViewletManager
+        # TODO Catch multiple managers in case two managers of the same specificity exist..
+        # Look up the viewletmanager most specific to this context.
+        # We're assuming that getAdapters returns the most specific last. Should this change...yikes!
+        name, manager = list(getSiteManager().getAdapters((self.grouping, self.request, self), IPersonViewletManager))[-1]
+        # Render the manager
+        manager.update()
+        self.renderedManager = manager.render()
         self.template = ViewPageTemplateFile('gallery.pt')
-        return self.template(self.context, self.request)
+        return self.template()
         
 class PersonTabularView(PersonView):
     def __init__(self, person, classification, request): # Override __init__ so we have 4 params instead of the usual 3 (since this is called using a multi-view lookup)
