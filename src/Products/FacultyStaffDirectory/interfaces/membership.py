fe1f9e49f975afee9f32862d5565aca6cb03eaa2
Erik Rose <ewr119@psu.edu>
2008-12-24 19:55:05 +0000

    Killed an unused import.

--- src/Products/FacultyStaffDirectory/interfaces/membership.py
+++ src/Products/FacultyStaffDirectory/interfaces/membership.py
@@ -4,8 +4,6 @@
 __author__ = """WebLion <support@weblion.psu.edu>"""
 __docformat__ = 'plaintext'
 
-from zope.interface import Interface
-
 from Products.FacultyStaffDirectory.interfaces import IPersonGrouping
 
 class IGroupingProvidingMembership(IPersonGrouping):
