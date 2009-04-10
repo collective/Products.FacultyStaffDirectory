109742b74cc40ad3a33d4d902a77750517b498ed
Erik Rose <ewr119@psu.edu>
2009-04-10 16:16:03 +0000

    spamProtectFSD is probably fixed now.

--- src/Products/FacultyStaffDirectory/skins/FacultyStaffDirectory/spamProtectFSD.py
+++ src/Products/FacultyStaffDirectory/skins/FacultyStaffDirectory/spamProtectFSD.py
@@ -3,6 +3,9 @@
 # Implement a different email obfuscating approach than the standard Plone spam
 # protection.  Dots, @ etc. will be replaced with a string representation.
 
+from Products.CMFCore.utils import getToolByName
+portal = getToolByName(context, 'portal_url').getPortalObject()
+
 if portal.restrictedTraverse('++fsdmembership++snork').getObfuscateEmailAddresses():
     email = email.replace('.', ' [ DOT ] ')
     email = email.replace('@', ' [ AT ] ')
