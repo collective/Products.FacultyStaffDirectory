25e3f3700887824b9fa40d7e5b280cfc176ba395
Erik Rose <ewr119@psu.edu>
2009-04-21 19:36:01 +0000

    Removed triplicate registration of a GenericSetup profile.

--- src/Products/FacultyStaffDirectory/__init__.py
+++ src/Products/FacultyStaffDirectory/__init__.py
@@ -47,12 +47,3 @@ def initialize(context):
         context.registerClass(meta_type   = all_ftis[i]['meta_type'],
                               constructors= (all_constructors[i],),
                               permission  = ADD_CONTENT_PERMISSIONS[klassname])
-
-    profile_registry.registerProfile( 
-        name='default', 
-        title=PROJECTNAME, 
-        description=u'Profile for FacultyStaffDirectory', 
-        path='profiles/default', 
-        product='FacultyStaffDirectory', 
-        profile_type=EXTENSION, 
-        for_=IPloneSiteRoot) 
