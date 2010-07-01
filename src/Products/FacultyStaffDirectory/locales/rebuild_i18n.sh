355427e6554ad5b0284087a70b96ca44c2a405c6
lyc5164 <lyc5164@localhost>
2010-07-01 17:30:10 +0000

    Added a bash script to generated news msgid for l10n

---
+++ src/Products/FacultyStaffDirectory/locales/rebuild_i18n.sh
@@ -0,0 +1,28 @@
+#i18ndude rebuild-pot --pot ./Content.pot \ 
+#                      --merge ../i18n/generated.pot \ 
+#                      --exclude=`find ../profiles -name "*.*py"` \
+#                      --create content ../ || exit 1
+
+rm ./rebuild_i18n.log
+
+i18ndude merge --pot ./manual.pot --merge ./generated.pot
+i18ndude rebuild-pot --pot ./FacultyStaffDirectory.pot --merge ./manual.pot --create FacultyStaffDirectory ../ || exit 1
+i18ndude sync --pot ./FacultyStaffDirectory.pot ./*/LC_MESSAGES/FacultyStaffDirectory.po
+
+i18ndude rebuild-pot --pot ../i18n/FacultyStaffDirectory-plone.pot --exclude=`../Specialty.py ../Person.py ../FacultyStaffDirectory.py ../interfaces/configuration.py` --create plone ../
+i18ndude sync --pot ../i18n/FacultyStaffDirectory-plone.pot ../i18n/FacultyStaffDirectory-plone-*.po
+
+WARNINGS=`find . -name "*pt" | xargs i18ndude find-untranslated | grep -e '^-WARN' | wc -l`
+ERRORS=`find . -name "*pt" | xargs i18ndude find-untranslated | grep -e '^-ERROR' | wc -l`
+FATAL=`find . -name "*pt"  | xargs i18ndude find-untranslated | grep -e '^-FATAL' | wc -l`
+
+echo
+echo "There are $WARNINGS warnings \(possibly missing i18n markup\)"
+echo "There are $ERRORS errors \(almost definitely missing i18n markup\)"
+echo "There are $FATAL fatal errors \(template could not be parsed, eg. if it\'s not html\)"
+echo "For more details, run \'find . -name \"\*pt\" \| xargs i18ndude find-untranslated\' or" 
+echo "Look the rebuild i18n log generate for this script called \'rebuild_i18n.log\' on locales dir" 
+
+touch ./rebuild_i18n.log
+
+find ../skins/ -name "*pt" | xargs i18ndude find-untranslated > rebuild_i18n.log
