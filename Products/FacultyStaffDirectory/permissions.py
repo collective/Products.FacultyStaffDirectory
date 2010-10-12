9ac7f8d4751f056726b95a95cf579cb4be5c6dba
cxe5014 <cxe5014@localhost>
2010-10-12 19:30:01 +0000

    added permission to control role inheritance

--- Products/FacultyStaffDirectory/permissions.py
+++ Products/FacultyStaffDirectory/permissions.py
@@ -5,3 +5,4 @@ ASSIGN_COMMITTIES_TO_PEOPLE = 'FacultyStaffDirectory: Assign Committees to Peopl
 ASSIGN_DEPARTMENTS_TO_PEOPLE = 'FacultyStaffDirectory: Assign Departments to People'
 CHANGE_ROLES = 'FacultyStaffDirectory: Change roles' # used to guard workflow transitions between 'active' and 'inactive'
 CHANGE_PERSON_IDS = 'FacultyStaffDirectory: Change Person IDs' # used to control write access to ID field of FSDPersons
+PROVIDES_ROLES = 'FacultyStaffDirectory: Provides Roles' # used to control role inheritance via workflow
