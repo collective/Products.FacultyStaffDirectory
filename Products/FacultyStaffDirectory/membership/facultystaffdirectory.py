0f5326bcfbe32e777405a9beb24f0f81b0d3139e
cxe5014 <cxe5014@localhost>
2010-10-12 19:29:12 +0000

    control roles via permission (set by workflow state)

--- Products/FacultyStaffDirectory/membership/facultystaffdirectory.py
+++ Products/FacultyStaffDirectory/membership/facultystaffdirectory.py
@@ -5,13 +5,16 @@ from Products.CMFCore.utils import getToolByName
 
 from Products.membrane.interfaces import IGroup
 from Products.membrane.interfaces import IMembraneUserAuth
+
 # from Products.membrane.at.interfaces import ICategoryMapper
 
 # from Products.membrane.config import ACTIVE_STATUS_CATEGORY
 from Products.membrane.config import TOOLNAME as MEMBRANE_TOOL
+
 # from Products.membrane.utils import generateCategorySetIdForType
 
 from Products.FacultyStaffDirectory.interfaces.facultystaffdirectory import IFacultyStaffDirectory
+from Products.FacultyStaffDirectory.permissions import PROVIDES_ROLES
 
 class Group(object):
     """Allow a FacultyStaffDirectory to act as a group for contained people
@@ -31,16 +34,11 @@ class Group(object):
         Return an empty list of roles if the directory is in a workflow state
         that is not active in membrane_tool.
         """
-        mb = getToolByName(self.context, MEMBRANE_TOOL)
-        wf = getToolByName(self.context, 'portal_workflow')
-        return self.context.getRoles()
-        # reviewState = wf.getInfoFor(self.context, 'review_state')
-        # wfmapper = ICategoryMapper(mb)
-        # categories = generateCategorySetIdForType(self.context.portal_type)
-        # if wfmapper.isInCategory(categories, ACTIVE_STATUS_CATEGORY, reviewState):
-        #     return self.context.getRoles()
-        # else:
-        #     return ()
+        pm = getToolByName(self.context, 'portal_membership')
+        if pm.checkPermission(PROVIDES_ROLES,self.context):
+            return self.context.getRoles()
+        else:
+            return ()
     
     def getGroupId(self):
         return self.context.getId()
