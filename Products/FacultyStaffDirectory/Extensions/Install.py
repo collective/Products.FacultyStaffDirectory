27a58ec1ae2d6a312438230df905566f0e7542f4
Eric Steele <ems174@psu.edu>
2011-02-25 21:30:21 +0000

    Blech.

--- Products/FacultyStaffDirectory/Extensions/Install.py
+++ Products/FacultyStaffDirectory/Extensions/Install.py
@@ -8,16 +8,8 @@ __docformat__ = 'plaintext'
 
 
 def install(self, reinstall=False):
-    """ External Method to install FacultyStaffDirectory """
-
-    def importProfiles(self, importContexts):
-        """Import all steps from the GenericSetup profiles listen in `importContexts`."""
-        setupTool = getToolByName(self, 'portal_setup')
-        for eachContext in importContexts:
-            setupTool.runAllImportStepsFromProfile(eachContext)
-
-    profilesToImport = ('profile-Products.FacultyStaffDirectory:default',)
-    importProfiles(self, profilesToImport)
+    setup_tool = getToolByName(self, 'portal_setup')
+    setup_tool.runAllImportStepsFromProfile('profile-Products.FacultyStaffDirectory:default')
 
 def uninstall(self, reinstall=False):
     setup_tool = getToolByName(self, 'portal_setup')
