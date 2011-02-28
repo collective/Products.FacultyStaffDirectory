b7d84b6691047c77ff74c1b89339c7e803bfeda1
Eric Steele <ems174@psu.edu>
2011-02-28 17:01:11 +0000

    Turns out we don't need the Install method now. BugzID: 2689

--- Products/FacultyStaffDirectory/Extensions/Install.py
+++ Products/FacultyStaffDirectory/Extensions/Install.py
@@ -1,14 +1,4 @@
-# -*- coding: utf-8 -*-
 from Products.CMFCore.utils import getToolByName
-
-
-__author__ = """WebLion <support@weblion.psu.edu>"""
-__docformat__ = 'plaintext'
-
-
-def install(self, reinstall=False):
-    setup_tool = getToolByName(self, 'portal_setup')
-    setup_tool.runAllImportStepsFromProfile('profile-Products.FacultyStaffDirectory:default')
 
 def uninstall(self, reinstall=False):
     if not reinstall:
