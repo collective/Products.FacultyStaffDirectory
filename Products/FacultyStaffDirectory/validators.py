2dc807d8b24397a9a7ad2f6af72ec83021310ea7
Eric Steele <ems174@psu.edu>
2011-07-27 20:57:10 +0000

    PEP8.

--- Products/FacultyStaffDirectory/validators.py
+++ Products/FacultyStaffDirectory/validators.py
@@ -6,14 +6,20 @@ from Products.FacultyStaffDirectory import FSDMessageFactory as _
 class SequenceValidator(object):
     """A wrapper that runs a validator on each item of a sequence-like Field.
     
-    For example, if you have a Lines field that should contain one URL per line, `SequenceValidator` can apply the `isURL` validator individually to each line:
-        Person_schema['websites'].validators = SequenceValidator('isURLs', validation.validatorFor('isURL'))
+    For example, if you have a Lines field that should contain one URL per line,
+    `SequenceValidator` can apply the `isURL` validator individually to each
+    line:
+        Person_schema['websites'].validators = 
+                   SequenceValidator('isURLs', validation.validatorFor('isURL'))
     """
     
     def __init__(self, name, validator, description='', **kw):
         """Constructor
         
-        validator - a validator (or ValidatorChain) to run against each item in the sequence. For reasonable results, make sure your chain cites the bad input in its error message. Otherwise, the user won't know what the error message applies to.
+        validator - a validator (or ValidatorChain) to run against each item in
+        the sequence. For reasonable results, make sure your chain cites the bad
+        input in its error message. Otherwise, the user won't know what the
+        error message applies to.
         """
         self.name = name
         self.title = kw.get('title', name)
@@ -26,7 +32,10 @@ class SequenceValidator(object):
         if errors:
             return '\n\n'.join(errors)
         else:
-            # Not sure why this needs to be True, but returning 1 (like RegexValidator) throws an Unsubscriptable Object exception. [Ed: It's because that's what the IValidator interface proclaims. The stock validators are just nonconformant.]
+            # Not sure why this needs to be True, but returning 1 (like
+            # RegexValidator) throws an Unsubscriptable Object exception. [Ed:
+            # It's because that's what the IValidator interface proclaims. The
+            # stock validators are just nonconformant.]
             return True
 
 classImplements(SequenceValidator, IValidator)
