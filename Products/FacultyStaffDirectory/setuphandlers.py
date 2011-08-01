0b6280dae005b4844c9650003d1524f715b04efd
Eric Steele <ems174@psu.edu>
2011-08-01 13:58:18 +0000

    Add < Plone 4.1 check, repositorytool.xml. BugzID: 3754

--- Products/FacultyStaffDirectory/setuphandlers.py
+++ Products/FacultyStaffDirectory/setuphandlers.py
@@ -65,18 +65,23 @@ TYPES_TO_VERSION = ('FSDPerson', 'FSDCommittee', 'FSDSpecialty')
 def installVersionedTypes(context):
     if context.readDataFile('installVersionedTypes.txt') is None:
         return
-    from Products.CMFEditions.setuphandlers import DEFAULT_POLICIES
-    portal = context.getSite()
-    portal_repository = getToolByName(portal, 'portal_repository')
-    versionable_types = list(portal_repository.getVersionableContentTypes())
-    for type_id in TYPES_TO_VERSION:
-        if type_id not in versionable_types:
-            # use append() to make sure we don't overwrite any
-            # content-types which may already be under version control
-            versionable_types.append(type_id)
-            for policy_id in DEFAULT_POLICIES:
-                portal_repository.addPolicyForContentType(type_id, policy_id)
-    portal_repository.setVersionableContentTypes(versionable_types)
+    try:
+        from Products.CMFEditions.setuphandlers import DEFAULT_POLICIES
+    except ImportError:
+        # Use repositorytool.xml instead (Plone 4.1 and above)
+        pass
+    else:
+        portal = context.getSite()
+        portal_repository = getToolByName(portal, 'portal_repository')
+        versionable_types = list(portal_repository.getVersionableContentTypes())
+        for type_id in TYPES_TO_VERSION:
+            if type_id not in versionable_types:
+                # use append() to make sure we don't overwrite any
+                # content-types which may already be under version control
+                versionable_types.append(type_id)
+                for policy_id in DEFAULT_POLICIES:
+                    portal_repository.addPolicyForContentType(type_id, policy_id)
+        portal_repository.setVersionableContentTypes(versionable_types)
 
 
 def uninstallNavTreeSettings(context):
