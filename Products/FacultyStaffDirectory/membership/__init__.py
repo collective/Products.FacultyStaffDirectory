fc2e3929baa710e2644f03413d3af81622bd1233
Craig Haynal <cah190@psu.edu>
2012-11-14 10:09:02 -0500

    Applying Laurence's patch to fix user enumeration issues due to Membrane limitations.  BugzID: 18418

--- Products/FacultyStaffDirectory/membership/__init__.py
+++ Products/FacultyStaffDirectory/membership/__init__.py
@@ -5,3 +5,12 @@ import facultystaffdirectory
 import committee
 import classification
 import department
+
+
+def not_a_user_object(obj):
+    """
+    Unfortunately IMembraneUserProperties inherits from IMembraneUserObject
+    causing group property providers to also be registered as user providers.
+    Register this as a more specific adapter on affected classes.
+    """
+    return None
