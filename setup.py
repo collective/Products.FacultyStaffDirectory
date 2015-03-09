01f74d87ba91b1718688a2ebbfe85933ca21b573
Gil Forcada <gforcada@gnome.org>
2015-03-09 23:32:45 +0100

    Rename .txt to .rst

--- setup.py
+++ setup.py
@@ -14,8 +14,8 @@ version = '3.2'
 setup(name='Products.FacultyStaffDirectory',
       version=version,
       description="Provides content types for creating and organizing personnel directories within educational institutions. Integrates with Plone's users and groups infrastructure and supports an extensibility framework for custom requirements.",
-      long_description=open("README.txt").read() + "\n\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      long_description=open("README.rst").read() + "\n\n" +
+                       open(os.path.join("docs", "HISTORY.rst")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
