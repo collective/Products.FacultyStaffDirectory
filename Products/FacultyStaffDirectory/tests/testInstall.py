50bc2a8b8dc0d076bbd1d435dc17585d3bbf11ce
gil-cano <gil.cano@gmail.com>
2016-10-22 12:11:16 -0500

    remove trailing spaces

--- Products/FacultyStaffDirectory/tests/testInstall.py
+++ Products/FacultyStaffDirectory/tests/testInstall.py
@@ -8,13 +8,10 @@ __docformat__ = 'plaintext'
 #
 
 from Products.CMFCore.utils import getToolByName
-
-# from Products.FacultyStaffDirectory.config import *
-# from Products.FacultyStaffDirectory.tests.testPlone import testPlone
 from Products.FacultyStaffDirectory.testing import INTEGRATION_TESTING
 
-
 import unittest
+
 
 originalMyFolderActionId = "mystuff"
 newMyFolderActionId = "fsdmystuff"
@@ -32,6 +29,7 @@ def checkKupuResourceList(tool, resourceType, portalTypeList):
             missingList.append(type)
     return missingList
 
+
 class testInstall(unittest.TestCase):
 
     layer = INTEGRATION_TESTING
@@ -47,7 +45,7 @@ class testInstall(unittest.TestCase):
         if installer.isProductInstalled('kupu'):
             self.has_kupu = True
             self.ktool = getToolByName(self.portal, 'kupu_library_tool')
-        
+
     def testMemberProfileSetup(self):
         cp = getToolByName(self.portal, 'portal_controlpanel')
         actions = cp.listActions()
@@ -72,7 +70,7 @@ class testInstall(unittest.TestCase):
             if fieldName not in self.pc.schema():
                 missingmetadata.append(fieldName)
         self.failIf(missingmetadata, 'Catalog is missing the following metadata fields: %s' % missingmetadata)
-        
+
     def testTopicIndexesAdded(self):
         missingindexes = []
         for index in ["getRawClassifications","getRawSpecialties","getRawCommittees","getRawPeople","getRawDepartments","getSortableName"]:
@@ -80,7 +78,7 @@ class testInstall(unittest.TestCase):
             if not idx or not idx.enabled:
                 missingindexes.append(index)
         self.failIf(missingindexes, 'ATCT Tool is missing the following indexes: %s' % missingindexes)
-        
+
     def testTopicMetadataAdded(self):
         missingmetadata = []
         for fieldName in ["getCommitteeNames","getDepartmentNames","getSpecialtyNames","getClassificationNames","getResearchTopics"]:
@@ -88,18 +86,18 @@ class testInstall(unittest.TestCase):
             if not md or not md.enabled:
                 missingmetadata.append(fieldName)
         self.failIf(missingmetadata, 'ATCT Tool is missing the following metadata fields: %s' % missingmetadata)
-        
+
     def testNavTreeSetup(self):
         missingmetatypes = []
         for mType in ['FSDCourse', 'FSDPerson', 'FSDFacultyStaffDirectoryTool']:
             if not mType in list(self.portal.portal_properties.navtree_properties.metaTypesNotToList):
                 missingmetatypes.append(mType)
         self.failIf(missingmetatypes, "The following FSD Types are still visible in the navtree and shouldn't be %s" % missingmetatypes)
-        
+
     def testConfigletAdded(self):
         cp = getToolByName(self.portal, 'portal_controlpanel')
         self.failIf("FacultyStaffDirectory" not in [ c.id for c in cp._actions ], 'FacultyStaffDirectory configlet has not been registered with the portal controlpanel')
-        
+
     def testVersioningSetup(self):
         missingversionable = []
         pr = getToolByName(self.portal, "portal_repository")
@@ -108,12 +106,12 @@ class testInstall(unittest.TestCase):
             if t not in pr.getVersionableContentTypes():
                 missingversionable.append(t)
         self.failIf(missingversionable, "%s are not listed as versionable and they should be" % missingversionable)
-        
+
         pMap = pr.getPolicyMap()
         for t in versionedTypes:
             self.failUnless(t in pMap)
             self.failUnless(pMap[t], [u'at_edit_autoversion', u'version_on_revert'])
-            
+
     def testKupuLinkableTypesSetup(self):
         if self.has_kupu:
             missingltypes = checkKupuResourceList(self.ktool, 'linkable', linkableKupuTypes)
@@ -129,7 +127,7 @@ class testInstall(unittest.TestCase):
             missingctypes = checkKupuResourceList(self.ktool, 'collection', collectionKupuTypes)
             self.failIf(missingctypes, '%s not listed as collection by Kupu' % missingctypes)
 
-        
+
 # class testUninstall(testPlone):
 #     def afterSetUp(self):
 #         migrationTool = getToolByName(self.portal, 'portal_migration')
@@ -143,7 +141,7 @@ class testInstall(unittest.TestCase):
 #             self.ktool = getToolByName(self.portal, 'kupu_library_tool')
 #         self.loginAsPortalOwner()
 #         installer.uninstallProducts(products=['FacultyStaffDirectory'])
-        
+
 #     def testMemberProfileTeardown(self):
 #         cp = getToolByName(self.portal, 'portal_controlpanel')
 #         actions = cp.listActions()
@@ -172,7 +170,7 @@ class testInstall(unittest.TestCase):
 #             if mType in list(self.portal.portal_properties.navtree_properties.metaTypesNotToList):
 #                 presentmetatypes.append(mType)
 #         self.failIf(presentmetatypes, "The following FSD Types are still listed in the navtree metatypesNotToList and they shouldn't be" % presentmetatypes)
-        
+
 #     def testActionIconsTeardown(self):
 #         ai = getToolByName(self.portal, 'portal_actionicons')
 #         try:
@@ -187,14 +185,14 @@ class testInstall(unittest.TestCase):
 #             if indexName in self.pc.indexes():
 #                 presentindexes.append(indexName)
 #         self.failIf(presentindexes, 'Catalog still holds the following indexes: %s' % presentindexes)
-        
+
 #     def testCatalogMetadataTeardown(self):
 #         presentmetadata = []
 #         for fieldName in ["getCommitteeNames", "getDepartmentNames", "getSpecialtyNames", "getClassificationNames", "getResearchTopics"]:
 #             if fieldName in self.pc.schema():
 #                 presentmetadata.append(fieldName)
 #         self.failIf(presentmetadata, 'Catalog still holds the following metadata fields: %s' % presentmetadata)
-        
+
 #     def testTopicIndexesTeardown(self):
 #         presentindexes = []
 #         allindexes = self.atct_tool.getIndexes()
@@ -202,7 +200,7 @@ class testInstall(unittest.TestCase):
 #             if index in allindexes:
 #                 presentindexes.append(index)
 #         self.failIf(presentindexes, 'ATCT Tool still has the following indexes: %s' % presentindexes)
-        
+
 #     def testTopicMetadataTeardown(self):
 #         presentmetadata = []
 #         allmetadata = self.atct_tool.getAllMetadata()
@@ -210,11 +208,11 @@ class testInstall(unittest.TestCase):
 #             if fieldName in allmetadata:
 #                 presentmetadata.append(fieldName)
 #         self.failIf(presentmetadata, 'ATCT Tool is missing the following metadata fields: %s' % presentmetadata)
-    
+
 #     def testConfigletTeardown(self):
 #         cp = getToolByName(self.portal, 'portal_controlpanel')
 #         self.failIf("FacultyStaffDirectory" in [ c.id for c in cp._actions ], 'FacultyStaffDirectory configlet is still registered with the portal controlpanel')
-        
+
 #     def testKupuLinkableTypesSetup(self):
 #         if self.has_kupu:
 #             missingltypes = checkKupuResourceList(self.ktool, 'linkable', linkableKupuTypes)
@@ -234,14 +232,14 @@ class testInstall(unittest.TestCase):
 #         skins = getToolByName(self.portal, 'portal_skins')
 #         for layerName in skins.getSkinSelections():
 #             self.failIf('FacultyStaffDirectory' in skins.selections[layerName], 'Skin layer not unregistered in layer %s' % layerName)
-        
+
 #     def testStepsUnregistered(self):
 #         setup = getToolByName(self.portal, 'portal_setup')
 #         registry = setup.getImportStepRegistry()
 #         remainingIds = [a['id'] for a in registry.listStepMetadata() if 'Products.FacultyStaffDirectory' in a['handler']]
 #         self.failIf(remainingIds, "The following import steps were not unregistered: %s" % remainingIds)
 
-        
+
 # class testReinstall(testPlone):
 #     def afterSetUp(self):
 #         migrationTool = getToolByName(self.portal, 'portal_migration')
@@ -249,9 +247,9 @@ class testInstall(unittest.TestCase):
 #         self.loginAsPortalOwner()
 #         self.directory = self.getPopulatedDirectory()
 #         self.person = self.getPerson(id='abc123', firstName="Test", lastName="Person")
-        
+
 #     def testUsersExistOnReinstall(self):
-        
+
 #         # Person/Member abc123 should exist in acl_users as a membrane user object
 #         acl = getToolByName(self.portal, 'acl_users')
 #         self.failUnless(acl.getUserById(id='abc123'))
@@ -260,35 +258,35 @@ class testInstall(unittest.TestCase):
 #         qi = getToolByName(self.portal, 'portal_quickinstaller')
 #         #qi.installProduct('FacultyStaffDirectory', reinstall=True)
 #         qi.reinstallProducts(products='FacultyStaffDirectory')
-        
+
 #         # abc123 should still exist in acl_users
 #         self.failUnless(acl.getUserById(id='abc123'))
-        
+
 # class testLargeDirectory(testPlone):
 #     def afterSetUp(self):
 #         self.loginAsPortalOwner()
 #         self.numPeople = 60
 #         self.directory = self.getPopulatedDirectory()
 #         self.person_ids = self.getLargeDirectory(self.directory, self.numPeople)
-        
+
 #     def testLargeDirSetup(self):
 #         numPeople = len(self.directory.getPeople())
 #         self.failUnlessEqual(numPeople, self.numPeople, "wrong number of people in the directory (%d), something is wrong" % numPeople)
-        
+
 #     def testLargeDirReinstall(self):
 #         """Benchmark and test reinstalling FSD with a directory holding a large number of people"""
 #         from time import time
 #         from random import choice
-        
+
 #         qi = getToolByName(self.portal, 'portal_quickinstaller')
 #         acl = getToolByName(self.portal, 'acl_users')
-        
+
 #         # pick a user and make sure they exist in acl_users before we start
 #         user_id = choice(self.person_ids)
 #         person = self.directory[user_id]
 #         self.failUnless(acl.getUserById(id=user_id),"Problem:  person is not listed in acl_users")
 #         self.failUnless(person.UID(),"Problem: expected person object %s to have a UID.  It does not" % person)
-        
+
 #         # how long does it take to reinstall FSD?
 #         import time
 #         start_time = time.time()
@@ -297,11 +295,11 @@ class testInstall(unittest.TestCase):
 #         elapsed_time = end_time-start_time
 #         reinstall_report = "\nreinstalling FSD with a directory containing %s people took %s seconds\n" % (self.numPeople, elapsed_time)
 #         print "\n" + ("*" * 20) + reinstall_report + ("*" * 20)
-        
+
 #         # test that a person in the FSD is still a user
 #         self.failUnless(acl.getUserById(id=user_id),"Problem:  after reinstall person is not listed in acl_users")
 #         self.failUnless(person.UID(),"Problem: after reinstall expected person object %s to have a UID.  It does not" % person)
-        
+
 
 # def test_suite():
 #     from unittest import TestSuite, makeSuite
@@ -310,5 +308,5 @@ class testInstall(unittest.TestCase):
 #     suite.addTest(makeSuite(testUninstall))
 #     suite.addTest(makeSuite(testReinstall))
 #     suite.addTest(makeSuite(testLargeDirectory))
-    
+
 #     return suite
